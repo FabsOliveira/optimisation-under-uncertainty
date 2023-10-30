113e40201beec4b876e16b0bc69759182c538f53
Fabricio Oliveira <fabricio.oliveira@aalto.fi>
2023-10-30 18:38:29 +0200

    Add basic course data

--- src/_data/course_info.jl
+++ src/_data/course_info.jl
@@ -1,13 +1,12 @@
 Dict(
-    "course_name" => "Name of your course",
-    "course_subtitle" => "a short catchy phrase about your course",
-    "code" => "code of your course",
-    "semester" => "Fall 20XX",
+    "course_name" => "Optimisation under uncertainty",
+    "course_subtitle" => "Stochastic programming and robust optimisation",
+    "code" => "MS-EV00XX",
+    "semester" => "Autumn 2024",
     "authors" => [
-        "First author name" => "first author homepage link",
-        "Second author name" => "second author homepage link"
+        "Fabricio Oliveira" => "gamma-opt.aalto.fi.fabricio-oliveira",
     ],
-    "institution" => "Your university (or similar) name",
+    "institution" => "Aalto University",
     "institution_logo" => "name of your institution logo file, e.g. `logo.svg`. Place this under `assets`",
     "repo" => "link to the repository of the source code"
 )
